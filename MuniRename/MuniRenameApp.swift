b3fca0d2bad89f256c539385a961253789b92bb2
Mathieu Beaudin <macthieu@me.com>
2025-10-07 19:32:01 -0400

    Initial Commit

---
+++ MuniRename/MuniRenameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuniRenameApp.swift
+//  MuniRename
+//
+//  Created by Mathieu Beaudin on 2025-10-07.
+//
+
+import SwiftUI
+
+@main
+struct MuniRenameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
